185b9ab15509aa7f79a2ebd39b4c0463dbe5cfbc
Auden Hinton <auden@outlook.com>
2024-04-18 07:24:18 -0700

    fix: build lib

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -6,11 +6,14 @@ interface TypeCMSConfig {
 interface EntryQuery {
     [key: string]: any;
 }
+interface EntryOptions {
+    format?: ['json', 'html'];
+}
 declare class TypeCMS {
     private baseUrl;
     private token;
     private projectId;
     constructor(obj: TypeCMSConfig);
-    getEntries(body: EntryQuery): Promise<any>;
+    getEntries(body: EntryQuery, options?: EntryOptions): Promise<any>;
 }
 export default TypeCMS;
